3238450b3b72e0784f80831a70a3edb9208b19ca
Adama Saho <adama.webdev@gmail.com>
2023-04-20 22:26:15 +0200

    first commit

---
+++ portfoliö/src/components/Portfolio/Data.jsx
@@ -0,0 +1,59 @@
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Joy Fitness",
+    category: "web",
+    href: "https://joy-fitness.netlify.app"
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Ecommerce Shop",
+    category: "app",
+    href: "https://haul-shop.netlify.app"
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "CinePlay",
+    category: "design",
+    href: "https://cine-play.netlify.app"
+  },
+  // {
+  //   id: 4,
+  //   image: Work4,
+  //   title: "App movil",
+  //   category: "app",
+  //   href: "https://www.google.de/?hl=de"
+  // },
+  // {
+  //   id: 5,
+  //   image: Work5,
+  //   title: "Web design",
+  //   category: "web",
+  //   href: "https://www.google.de/?hl=de"
+  // },
+];
+
+// projects
+export const projectsNav = [
+  // {
+  //   name: "all",
+  // },
+  // {
+  //   name: "web",
+  // },
+  // {
+  //   name: "app",
+  // },
+  // {
+  //   name: "design",
+  // },
+];
---
+++ portfoliö/src/components/header/Header.jsx
@@ -0,0 +1,164 @@
+import React, { useState } from "react";
+import "./header.css";
+import { Link } from 'react-scroll';
+
+
+ 
+
+const Header = () => {
+  /*=============== Change Background Header ===============*/
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    // when the scroll is higher than 200 viewport height, add the scroll-header class to a tag with the header tag
+    if (this.scrollY >= 80) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+  });
+
+  /*=============== Toggle Menu ===============*/
+  const [Toggle, showMenu] = useState(false);
+  const [activeNav, setActiveNav] = useState("#home");
+
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Portfolio
+        </a>
+
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <Link
+
+to="home"
+  spy={true}
+  smooth={true}
+  offset={-70}
+  duration={500}
+
+                href="#home"
+                onClick={() => setActiveNav("#home")}
+                className={
+                  activeNav === "#home" ? "nav__link active-link" : "nav__link"
+                }
+              >
+                <i className="uil uil-estate nav__icon"></i> Home
+              </Link>
+            </li>
+
+            <li className="nav__item">
+              <Link
+
+to="portfolio"
+spy={true}
+smooth={true}
+offset={-70}
+duration={500}
+
+                href="#portfolio"
+                onClick={() => setActiveNav("#portfolio")}
+                className={
+                  activeNav === "#portfolio"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-scenery nav__icon"></i> Projekte
+              </Link>
+            </li>
+            
+
+            <li className="nav__item">
+              <Link
+
+to="skills"
+  spy={true}
+  smooth={true}
+  offset={-70}
+  duration={500}
+
+                href="#skills"
+                onClick={() => setActiveNav("#skills")}
+                className={
+                  activeNav === "#skills"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </Link>
+            </li>
+
+            {/* <li className="nav__item">
+              <a
+                href="#services"
+                onClick={() => setActiveNav("#services")}
+                className={
+                  activeNav === "#services"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
+            </li> */}
+
+          
+
+            <li className="nav__item">
+              <Link
+
+to="about"
+  spy={true}
+  smooth={true}
+  offset={-70}
+  duration={500}
+
+                href="#about"
+                onClick={() => setActiveNav("#about")}
+                className={
+                  activeNav === "#about" ? "nav__link active-link" : "nav__link"
+                }
+              >
+                <i className="uil uil-user nav__icon"></i> About
+              </Link>
+            </li>
+
+
+            <li className="nav__item">
+              <Link
+
+to="contact"
+  spy={true}
+  smooth={true}
+  offset={-70}
+  duration={500}
+
+                href="#contact"
+                onClick={() => setActiveNav("#contact")}
+                className={
+                  activeNav === "#contact"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uil uil-message nav__icon"></i> Kontakt
+              </Link>
+            </li>
+          </ul>
+
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
+        </div>
+
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
